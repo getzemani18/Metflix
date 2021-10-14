3e620804ebb85cd5253b16eec5873dc3007043b2
getzemani18 <amandaalvarado0129@gmail.com>
2021-10-14 17:11:29 -0500

    Opcion buscar y empezando la primera pantalla

--- src/data.js
+++ src/data.js
@@ -1,10 +1,29 @@
-//APi peliculas 
-export let obtenerserie = (searchSerie)  => {
-fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=373d6e23&s=${searchSerie}`)
-.then((response)=> response.json())
-.then((data) => console.log(data))
-.catch((error) => console.log(error))
-.finally(() => console.log("promesa resuelta"))
+/*
+let renderSeries= (SeriesData) => { // Treamos la data del export
+    console.log(SeriesData)
+    // llamamos desde el id a el nombre de la serie desde HTML
+    let serietitle = document.getElementById("serietitle")
+    // llamamos desde un id a una imagen  desde HTML
+    let serieImg = document.getElementById("serieimg")
+    //llamamos dede un id a el poster
+    let serieyear = document.getElementById("serieyear")
+    serietitle.innerHTML = SeriesData.Title
+    serieImg.setAttribute("src", SeriesData.Poster)
+    serieyear.setAttribute("src", SeriesData.Year)
+
 }
 
-//exportar peliculas desde app.js
+
+
+//APi peliculas 
+export let obtenerserie = (searchSerie) => { //Definimos la funcion con una documentacion "searchserie"
+    fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=3067e0bb&s=${searchSerie}`)  // fech es una promesa //$sintaxis que significa que no es un string, que es una variable
+    // Entonces... llamamos a los datos  
+   .then((response) => response.json())
+   .then( (data) => renderSeries(data))
+   .catch((error) => console.log(error))
+   // Finalizamos la promesa
+   .finally(() => console.log("promesa resuelta"))
+  }
+
+*/
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import {obtenerserie} from "/data.js"
+/*
+import {obtenerserie} from "./data.js" // importamos el data.js
 
-let searchSerie = () => {
-    let searchValue = document.getElementById("input").value 
-    console.log(searchValue)
+let buscarserie = () => { // Hacemos una funcion  para traer el id
+    let searchValue = document.getElementById("input").value // traemos el id del HTML,
+    console.log(searchValue) //checamos si sale la funcion
     obtenerserie(searchValue)
 }
 
-let buttonSerie = document.getElementById('buttonbuscar')
-buttonSerie.addEventListener('click', searchSerie)
-obtenerserie()
+let buttonSerie= document.getElementById('buttonbuscar') // nos traemos el botonbuscar 
+buttonSerie.addEventListener('click', buscarserie) // para que al dar click busque lo que buscas
+*/
